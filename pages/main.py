b30f8f143c66125a6467d736b81f8fab1d1c90df
maksat sarsenbaev <88890025m@gmail.com>
2024-04-05 14:14:37 +0600

    Initial commit

---
+++ pages/main.py
@@ -0,0 +1,35 @@
+import datetime as dt
+import pickle
+
+import pandas as pd
+import streamlit as st
+
+
+with open('models/XGBRegressor.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+st.set_page_config(page_title='Predict', page_icon='🎰')
+
+st.write('__Введите данные для предсказания 👇__')
+
+d = pd.Timestamp(st.date_input('__Введите дату__'))
+t = st.time_input('__Введите время__')
+
+inp = {
+    'hour': t.hour,
+    'dayofweek': (d.dayofweek + 2) % 7,
+    'quarter': d.quarter,
+    'month': d.month,
+    'year': d.year,
+    'dayofyear': d.dayofyear,
+    'dayofmonth': d.day
+}
+df_inp = pd.DataFrame([inp])
+
+st.write(
+    '## Для следующего набора данных:', 
+    df_inp, 
+    '## Получаем следующее предсказание:'
+    )
+
+st.write(f'### Расчетное энергопотребление в мегаваттах (МВт): {model.predict(df_inp)} ⚡')
